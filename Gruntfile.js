4a49e7e1e60927b1601f44b068382da4bf5884fc
Rob Odil <rodil@godaddy.com>
2013-11-08 18:43:12 -0700

    Added a bunch of grunt stuff

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('load-grunt-tasks')(grunt, 'grunt-*');
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {},
+            vendor: {
+                src: [
+                    'src/vendor/jquery/jquery.min.js',
+                    'src/vendor/modernizr/modernizr.js',
+                    'src/vendor/handlebars/handlebars.js',
+                    'src/vendor/ember/ember.min.js'
+                ],
+                dest: 'staticfiles/assets/vendor.js'
+            },
+            core: {
+                src: [
+                    '.tmp/templates.js',
+                    'src/application/*.js',
+                    'src/application/(controller,model,view)/*.js'
+                ],
+                dest: 'staticfiles/assets/core.js'
+            }
+        },
+        emberTemplates: {
+            compile: {
+                options: {
+                    amd: false,
+                    templateBasePath: /src\/application\/_templates\//
+                },
+                files: {
+                    '.tmp/templates.js': 'src/application/_templates/*.hbs'
+                }
+            }
+        },
+        less: {
+            core: {
+                options: {
+                    yuicompress: true
+                },
+                files: {
+                    "staticfiles/assets/core.css": [
+                        "src/application/_less/*.less"
+                    ]
+                }
+            }
+        },
+        jshint: {
+            all: 'src/application/!(tests)/*.js'
+        },
+        watch: {
+            css: {
+                files: [
+                    'src/application/_less/*.less'
+                ],
+                tasks: [
+                    'less.core'
+                ],
+                options: {
+                    spawn: false
+                }
+            },
+            js: {
+                files: [
+                    'src/application/**/*.js',
+                    'src/application/_templates/*.hbs'
+                ],
+                tasks: [
+                    'emberTemplates',
+                    'uglify'
+                ],
+                options: {
+                    spawn: false
+                }
+            },
+            server: {
+                files: ['server.js'],
+                tasks: [],
+                options: {
+                    spawn: false
+                }
+            }
+        },
+        nodemon: {
+            prod: {
+                options: {
+                    file: 'server.js',
+                    ignoredFiles: ['README.md', 'node_modules/**', 'src/**', 'staticfiles/**', 'Gruntfile.js'],
+                    watchedExtensions: ['js'],
+                    watchedFolders: ['./'],
+                    delayTime: 1,
+                    legacyWatch: true,
+                    env: {
+                        ENVIRONMENT: 'dev',
+                        PORT: '8081'
+                    },
+                    cwd: __dirname
+                }
+            },
+            exec: {
+                options: {
+                    exec: 'less'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('dev', function () {
+        grunt.task.run([
+            'emberTemplates',
+            'less',
+            'uglify.core',
+            'watch'
+        ]);
+    });
+    grunt.registerTask('vendor', function () {
+        grunt.task.run([
+            'uglify.vendor'
+        ]);
+    });
+
+};
---
+++ staticfiles/assets/core.js
@@ -0,0 +1,1 @@
+Ember.TEMPLATES.application=Ember.Handlebars.template(function(a,b,c,d,e){this.compilerInfo=[4,">= 1.0.0"],c=this.merge(c,Ember.Handlebars.helpers),e=e||{},e.buffer.push("<h1>Hello</h1>")});
