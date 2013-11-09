78f770c18644f46adec42b6b95cc0c59afa846aa
Bryce Hill <bahill@godaddy.com>
2013-11-08 20:47:02 -0700

    add path to book schema

--- models/Book.js
+++ models/Book.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose'),
     Schema   = mongoose.Schema;
 
 var Book = new Schema({
-    title: String
+    title: String,
+    path: String
 });
 
 module.exports = mongoose.model('Book', Book);
