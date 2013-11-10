9c6228a8deab14ef05f2b21dcb9cdbbdf545d3a9
Rob Odil <rodil@godaddy.com>
2013-11-09 22:26:48 -0700

    Got edit page lay out framed up.

--- src/application/routes/book.js
+++ src/application/routes/book.js
@@ -1,19 +1,8 @@
 TandemApplication.reopen({
     BookRoute: Ember.Route.extend({
         setupController: function (controller, model) {
+            model.set('chaptersOpen', false);
             controller.set('book', model);
         }
-        // model: function (params) {
-        //     var promise = $.Deferred();
-
-        //     promise.resolve([{title: 'Monkey', id: '527e8328a42ea36868000001'}]);
-        //     console.log(arguments);
-        //     // return {
-        //     //   title: "Tomster",
-        //     //   url: "http://emberjs.com/images/about/ember-productivity-sm.png"
-        //     // };
-
-        //     return promise;
-        // }
     })
 });
--- src/application/view/book.js
+++ src/application/view/book.js
@@ -1,7 +1,6 @@
 TandemApplication.reopen({
     BookView: TandemApplication.BaseView.extend({
         init: function () {
-            console.log('view init');
             this._super();
         },
         templateName: 'book'
