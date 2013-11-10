4f17c96db91e6ee3718d218eced7cef148de79f4
brycehill <bryceahill@gmail.com>
2013-11-09 23:54:37 -0700

    added error handling

--- src/application/application.js
+++ src/application/application.js
@@ -5,6 +5,10 @@ var TandemApplication = Ember.Application.extend({
         this.get('socket').on('setupUser', function(user) {
         	that.set('author', that.AuthorModel.create(user._json));
         });
+        this.get('socket').on('error', function(data) {
+            // console.error(data.message);
+            that.set('error', data.message);
+        })
     }
 });
 
