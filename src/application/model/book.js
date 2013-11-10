a4f3f66d8922dd6c208f6a957f9b18475d45ae95
Micah <micah.wood@gmail.com>
2013-11-09 19:18:49 -0700

    semi random book colors (colours) for all

--- src/application/model/book.js
+++ src/application/model/book.js
@@ -1,7 +1,17 @@
 TandemApplication.reopen({
 	BookModel: Ember.Object.extend({
-		displayAuthor: function() {
-			return '1234';
-		}.property('author')
+		displayShortTitle: function() {
+			var title = this.get('title');
+			if (title.length > 40) {
+				title = title.substring(0, 40) + '...';
+			}
+			return title;
+		}.property('title'),
+		background: function() {
+			colors = ['#A6592D', '#A64029', '#400101', '#A62929', '#57493E', '#E4DDBD', '#A6A38C', '#316481', '#437DA5'];
+			background = 'background-color:';
+			background += colors[Math.floor(Math.random() * colors.length)];
+			return background;
+		}.property('background')
 	})
 });
