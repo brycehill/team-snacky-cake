27bba416961168265108b6532e90addaff8bce0e
Micah <micah.wood@gmail.com>
2013-11-10 15:22:52 -0700

    fixed logic for new book form

--- src/application/controller/application.js
+++ src/application/controller/application.js
@@ -5,7 +5,7 @@ TandemApplication.reopen({
                 var that = this;
             	var book = {};
             	book.title = this.get('bookName');
-            	if (book.title != '') {
+            	if (book.title != null) {
                 	TandemApp.get('socket').emit('addBook', book);
                     this.set('bookName', '');
              	}
